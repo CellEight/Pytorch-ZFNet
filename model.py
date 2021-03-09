e1da129af98764d8865cddf7d319f0d695353a8a
Lewis Patten <lp921@york.ac.uk>
2021-03-09 15:30:43 +0000

    finished model, added data

--- model.py
+++ model.py
@@ -1,33 +1,32 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class AlexNet(nn.Module):
-    """ A implementation of the AlexNet architecture from the paper 'ImageNet 
-        Classification with Deep Convolutional Neural Networks' """
+class ZFNet(nn.Module):
+    """ A implementation of the ZFNet architecture from the paper 'Visualizing and 
+        Understanding Convolutional Networks' by Zeiler and Fergus"""
     def __init__(self):
         super().__init__()
-        # The paper specifies that this layer should be stride 4 however this does not work with 
-        # the input dimensions, I work under the assumption that the definition of stride used 
-        # in the Alexnet paper and the pytorch library differ.
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=2, padding=5, padding_mode='reflect') 
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=7, stride=2, padding=5, padding_mode='reflect') 
         self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, padding_mode='reflect')
         self.conv3 = nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
         self.conv4 = nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
         self.conv5 = nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
-        self.fc6 = nn.Linear(43264,4096)
+        self.fc6 = nn.Linear(9216,4096)
         self.fc7 = nn.Linear(4096,4096)
         self.fc8 = nn.Linear(4096,1000)
-        self.pool = nn.MaxPool2d(3,2)
+        self.pool1 = nn.MaxPool2d(3,stride=2)
+        self.pool2 = nn.MaxPool2d(3,stride=2)
+        self.drop = nn.Dropout(0.5)
         self.drop = nn.Dropout(0.5)
         self.lrn = nn.LocalResponseNorm(size=5,alpha=10e-4,beta=0.75,k=2.0)
 
     def forward(self, x):
-        x = self.lrn(self.pool(F.relu(self.conv1(x))))
-        x = self.lrn(self.pool(F.relu(self.conv2(x))))
+        x = self.lrn(self.pool1(F.relu(self.conv1(x))))
+        x = self.lrn(self.pool2(F.relu(self.conv2(x))))
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
-        x = self.pool(F.relu(self.conv5(x)))
-        x = x.view(-1,43264)
+        x = self.pool2(F.relu(self.conv5(x)))
+        x = x.view(-1,9216)
         x = F.relu(self.drop(self.fc6(x)))
         x = F.relu(self.drop(self.fc7(x)))
         x = self.fc8(x)
